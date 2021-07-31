6fc6473561e8eb5be2db413e074d5a9336a9782f
Халим Магомедов <m.h.axn@mail.ru>
2021-07-31 14:20:16 +0300

    add logic WelcomeActivity

--- app/src/main/java/com/example/languagemanualitschoolsamsung/WelcomeActivity.java
+++ app/src/main/java/com/example/languagemanualitschoolsamsung/WelcomeActivity.java
@@ -1,13 +1,31 @@
 package com.example.languagemanualitschoolsamsung;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private Button signUpButton;
+    private TextView signInTextView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initViews();
+    }
+
+    private void initViews() {
+        signUpButton = findViewById(R.id.sign_up_button_welcome);
+        signInTextView = findViewById(R.id.sign_in_text_welcome);
+
+        signUpButton.setOnClickListener(v -> {
+            // TODO добавить переход на экран "SignUp"
+        });
+        signInTextView.setOnClickListener(v -> {
+            // TODO добавить переход на экран "SignIn"
+        });
     }
 }
